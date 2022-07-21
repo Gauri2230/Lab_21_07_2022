96179a1ae8f658d74d10d7cd01a4b3fa5337fa0b
Gauri2230 <108929783+Gauri2230@users.noreply.github.com>
2022-07-21 16:53:41 +0530

    Add files via upload

---
+++ Program3.java
@@ -0,0 +1,26 @@
+class Program3
+{
+	public static void main(String args[])
+	{
+		char i='Q';
+		int c= (char) i;
+		double d=323.142;
+		int j=(int) d;
+		double f = 72.38;
+		int k=(int) f;
+		int b=(byte) d;
+		System.out.print("i="+c);
+		System.out.println(",c="+i);
+		System.out.print("d="+d);
+		System.out.println(",j="+j);
+		System.out.print("f="+f);
+		System.out.println(",k="+k);
+		System.out.print("d="+d);
+		System.out.println(",b="+b);
+	}
+}
+
+		
+		
+		
+		
