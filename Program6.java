e22c1b5a993ba34cec03437550006ef78d15eb08
Gauri2230 <108929783+Gauri2230@users.noreply.github.com>
2022-07-21 17:39:41 +0530

    Add files via upload

---
+++ Program6.java
@@ -0,0 +1,11 @@
+class Program6
+{
+	public static void main(String args[])
+	{
+		int num=-20;
+		if (num>0)
+			System.out.println("-20 is Positive number");
+		else
+			System.out.println("-20 is Negative number");
+	}
+}
